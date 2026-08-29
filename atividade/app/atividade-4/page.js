c3b769caceaecdce22811efee8a486799aa7df84
Rosalvinnn <rosalvo.00000854909@unicap.br>
2026-08-29 02:04:40 +0000

    atividade-4.5

--- atividade/app/atividade-4/page.js
+++ atividade/app/atividade-4/page.js
@@ -1,3 +1,4 @@
+
 import styles from "./page.module.css";
 
 export default function Atividade4() {
@@ -7,31 +8,31 @@ export default function Atividade4() {
 
       <div className={styles.produtos}>
         <div className={styles.produto}>
-          <img src="/tenis1.jpg" alt="Tênis esportivo" />
+          <img src="/esportivo.wepb" alt="Tênis esportivo" />
           <h2>Tênis Esportivo</h2>
           <p>R$ 199,90</p>
         </div>
 
         <div className={styles.produto}>
-          <img src="/tenis2.jpg" alt="Tênis casual" />
+          <img src="/casual.wepb" alt="Tênis casual" />
           <h2>Tênis Casual</h2>
           <p>R$ 159,90</p>
         </div>
 
         <div className={styles.produto}>
-          <img src="/tenis3.jpg" alt="Tênis de corrida" />
+          <img src="/corrida.wepb" alt="Tênis de corrida" />
           <h2>Tênis de Corrida</h2>
           <p>R$ 249,90</p>
         </div>
 
         <div className={styles.produto}>
-          <img src="/tenis4.jpg" alt="Tênis urbano" />
+          <img src="/urbano.wepb" alt="Tênis urbano" />
           <h2>Tênis Urbano</h2>
           <p>R$ 179,90</p>
         </div>
 
         <div className={styles.produto}>
-          <img src="/tenis5.jpg" alt="Tênis clássico" />
+          <img src="/classico.wepb" alt="Tênis clássico" />
           <h2>Tênis Clássico</h2>
           <p>R$ 219,90</p>
         </div>
