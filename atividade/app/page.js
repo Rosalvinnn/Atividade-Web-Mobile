816ea4ad2b24dbebc38998aaa290a4c56f74e7dc
Rosalvinnn <rosalvo.00000854909@unicap.br>
2026-08-29 02:24:12 +0000

    all

--- atividade/app/page.js
+++ atividade/app/page.js
@@ -1,9 +1,16 @@
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <h1>Primeiro exercício de CSS</h1>
+
+      <div className={styles.links}>
+        <a href="https://atividade-six-phi.vercel.app/atividade-2">Atividade 2</a>
+        <a href="https://atividade-six-phi.vercel.app/atividade-3">Atividade 3</a>
+        <a href="https://atividade-six-phi.vercel.app/atividade-4">Atividade 4</a>
+      </div>
     </main>
   );
 }
